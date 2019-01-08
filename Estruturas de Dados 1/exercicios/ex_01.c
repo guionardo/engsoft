a182a133d21bf33819869cd9598a548eecc7e184
Guionardo <guionardo@gmail.com>
2019-01-07 23:24:11 -0200

    Estruturas de Dados

---
+++ Estruturas de Dados 1/exercicios/ex_01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define tamanho 5
+int main(){
+    int notas_alunosED[tamanho];
+    int notas_alunosBD[tamanho];
+    int notas_alunosES[tamanho];
+    printf("Guionardo\n");
+}
